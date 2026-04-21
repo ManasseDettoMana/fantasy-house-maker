3fdf7715add4f63f01e4af93526b407c7ad78125
ManasseDettoMana <smarrazzomanasse@gmail.com>
2026-04-21 20:48:48 +0200

    some shit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginForm } from './login/login-form/login-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginForm },
+    { path: '', component: LoginForm },
+    { path: '**', component: LoginForm },
+];
