880772c71aa8c0186bb01ac7bff1b16da5a395d8
ManasseDettoMana <smarrazzomanasse@gmail.com>
2026-04-21 21:22:23 +0200

    aa

--- frontend/src/app/login/login-form/login-form.ts
+++ frontend/src/app/login/login-form/login-form.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import {FormControl, ReactiveFormsModule, FormGroup, Validators, FormBuilder} from '@angular/forms';
+import { AuthService } from '../../core/services/auth.service';
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.html',
   styleUrl: './login-form.scss',
 })
 export class LoginForm {
+  private readonly fb = inject(FormBuilder);
+  isLoggedIn = false;
 
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  onSubmit(): void{
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.loginForm.value);
+    this.isLoggedIn = true;
+  }
 }
